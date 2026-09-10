45981b96d3df6d8f95453fc557c382e92852c524
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-10 14:47:17 -0400

    chore: version 0.51.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.otaviobarreto.pokedex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50
-        versionName = "0.50.0"
+        versionCode = 51
+        versionName = "0.51.0"
     }
 
     signingConfigs {
