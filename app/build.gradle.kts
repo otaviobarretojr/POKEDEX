e1b55a124f7d8e1bc6fcc8a9db048408b034a4d3
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-10 19:30:57 -0400

    release: bump app to 0.74.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.otaviobarreto.pokedex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 73
-        versionName = "0.73.0"
+        versionCode = 74
+        versionName = "0.74.0"
     }
 
     signingConfigs {
