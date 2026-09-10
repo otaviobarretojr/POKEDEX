d0b7b9de683443400b2d89e559d2e4e78f4574b8
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-10 07:21:53 -0400

    chore: bump app version to 0.27.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.otaviobarreto.pokedex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.26.0"
+        versionCode = 27
+        versionName = "0.27.0"
     }
 
     signingConfigs {
