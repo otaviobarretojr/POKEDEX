1e39270aee5e2ce58106725521a943f4db746234
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-10 19:48:19 -0400

    release: promote app to 1.0.0 stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.otaviobarreto.pokedex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 79
-        versionName = "0.79.0"
+        versionCode = 100
+        versionName = "1.0.0"
     }
 
     signingConfigs {
