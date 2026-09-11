39c94f88b26e6f3879ef303a1922ed3ee8cbb4d1
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-10 22:19:17 -0400

    test: enable JVM regression tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    testImplementation("junit:junit:4.13.2")
 }
