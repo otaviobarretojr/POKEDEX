dc2c9e53a34669301b774eb5d71f48de26627804
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-11 22:52:16 -0400

    fix: import dp for polished bottom navigation

--- app/src/main/java/com/otaviobarreto/pokedex/MainActivity.kt
+++ app/src/main/java/com/otaviobarreto/pokedex/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
 import androidx.navigation.navArgument
