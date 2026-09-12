abd8c40894a8ca91f7515b16b137cbe2964caabb
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-12 09:31:37 -0400

    release: set POKEDEX v18.0.0 stable version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.otaviobarreto.pokedex"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16100
-        versionName = "16.1.0"
+        versionCode = 18000
+        versionName = "18.0.0"
     }
 
     signingConfigs {
