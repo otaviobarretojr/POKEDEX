95c8dbae8acae0fe8c6643c011a4fcbe6dcc5c85
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-13 19:24:14 -0400

    qa: accept v20.10.0 release metadata

--- scripts/verify_repo.py
+++ scripts/verify_repo.py
@@ -1151,7 +1151,8 @@ local_v2070 = 'versionName = "20.7.0"' in local_gradle and "versionCode = 20700"
 local_v2071 = 'versionName = "20.7.1"' in local_gradle and "versionCode = 20701" in local_gradle
 local_v2080 = 'versionName = "20.8.0"' in local_gradle and "versionCode = 20800" in local_gradle
 local_v2090 = 'versionName = "20.9.0"' in local_gradle and "versionCode = 20900" in local_gradle
-if not (local_v1610 or local_v1611 or local_v1612 or local_v1613 or local_v1614 or local_v1615 or local_v1620 or local_v1700 or local_v1800 or local_v1810 or local_v1820 or local_v1830 or local_v1840 or local_v1841 or local_v1842 or local_v1850 or local_v1851 or local_v1900 or local_v1901 or local_v1910 or local_v1920 or local_v2000 or local_v2030 or local_v2040 or local_v2050 or local_v2060 or local_v2070 or local_v2071 or local_v2080 or local_v2090):
+local_v2100 = 'versionName = "20.10.0"' in local_gradle and "versionCode = 21000" in local_gradle
+if not (local_v1610 or local_v1611 or local_v1612 or local_v1613 or local_v1614 or local_v1615 or local_v1620 or local_v1700 or local_v1800 or local_v1810 or local_v1820 or local_v1830 or local_v1840 or local_v1841 or local_v1842 or local_v1850 or local_v1851 or local_v1900 or local_v1901 or local_v1910 or local_v1920 or local_v2000 or local_v2030 or local_v2040 or local_v2050 or local_v2060 or local_v2070 or local_v2071 or local_v2080 or local_v2090 or local_v2100):
     violations.append("Local build version is not aligned with supported releases")
 
 if (root / ".github/workflows/import-home-audio.yml").exists():
