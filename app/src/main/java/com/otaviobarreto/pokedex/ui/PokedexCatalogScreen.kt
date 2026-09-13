63d14d6ed0cd80864755ec96b22b9ddde9facac8
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-13 08:24:06 -0400

    fix: import interaction source for premium cards

--- app/src/main/java/com/otaviobarreto/pokedex/ui/PokedexCatalogScreen.kt
+++ app/src/main/java/com/otaviobarreto/pokedex/ui/PokedexCatalogScreen.kt
@@ -1,6 +1,7 @@
 package com.otaviobarreto.pokedex.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
