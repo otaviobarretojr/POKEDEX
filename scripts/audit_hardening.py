59c0b4723ebc7f65cd564e7c6066657b386eba01
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-13 16:13:13 -0400

    qa: support v20.8.0 release contract

--- scripts/audit_hardening.py
+++ scripts/audit_hardening.py
@@ -83,7 +83,8 @@ supported_version = (
     ('versionCode = 20500' in gradle and 'versionName = "20.5.0"' in gradle) or
     ('versionCode = 20600' in gradle and 'versionName = "20.6.0"' in gradle) or
     ('versionCode = 20700' in gradle and 'versionName = "20.7.0"' in gradle) or
-    ('versionCode = 20701' in gradle and 'versionName = "20.7.1"' in gradle)
+    ('versionCode = 20701' in gradle and 'versionName = "20.7.1"' in gradle) or
+    ('versionCode = 20800' in gradle and 'versionName = "20.8.0"' in gradle)
 )
 if not supported_version:
     errors.append("supported version contract changed unexpectedly")
