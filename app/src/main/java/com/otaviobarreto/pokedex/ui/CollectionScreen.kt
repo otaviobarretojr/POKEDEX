90b0eebd389cce651f1c297a2fda9f388bbef48b
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-20 14:45:26 -0400

    fix: import LazyRow for generations portal

--- app/src/main/java/com/otaviobarreto/pokedex/ui/CollectionScreen.kt
+++ app/src/main/java/com/otaviobarreto/pokedex/ui/CollectionScreen.kt
@@ -4,6 +4,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
